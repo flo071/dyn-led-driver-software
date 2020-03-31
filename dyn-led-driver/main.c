7e05613e6bac2d3f0c263f9b0eedf7a3e4f28fe9
unknown <flo071@gmx.at>
2020-03-31 19:46:58 +0200

    main.c

--- dyn-led-driver/main.c
+++ dyn-led-driver/main.c
@@ -41,6 +41,13 @@ void dac_set_current(void);
 void adc_measure_current(void);
 // Funktionsprototypen - Ende
 
+// Nachrichten Variablen - Start
+char lmm_message_transmit[64];
+char ucd_message_receive[64];
+char ucd_message_transmit[64];
+char pot_messege_transmit[16];
+// Nachrichten Variablen - Ende
+
 void init(void) {
 	spi_init();
 	can_init();
@@ -65,7 +72,13 @@ void spi_init(void) {
 	AVR32_GPIO.port[3].gper = 0<<01;
 	AVR32_GPIO.port[3].gper = 0<<02;
 	AVR32_GPIO.port[3].gper = 0<<03;
-	AVR32_GPIO.port[3].pmr0 = 0; AVR32_GPIO.port[3].pmr1 = 0; AVR32_GPIO.port[3].pmr2 = 0; // Port D Pin 0 CANRX MODE A
+	
+	AVR32_SPI0.CR.spien = 1; // Enable SPI
+	AVR32_SPI0.MR.ps = 0;    // Select peripheral device directly
+	AVR32_SPI0.MR.mstr = 1;  // SPI Master Mode
+	
+	// Data for Transmission is written to this register:
+	// AVR32_SPI0.TDR.td
 }
 
 // CANIF0/1 Schnittstelle initialisieren
@@ -92,7 +105,7 @@ void can_init(void) {
 	AVR32_GPIO.port[2].pmr0 = 1<<16; AVR32_GPIO.port[2].pmr1 = 1<<16; AVR32_GPIO.port[2].pmr2 = 0; // Port C Pin 16 CANTX MODE D
 	// CANIF channel 1
 	AVR32_CANIF.channel[1].CANCFG.ovrm  = 1; // Overrun Mode
-	AVR32_CANIF.channel[1].CANCFG.cmode = 2; // Loopback Mode
+	AVR32_CANIF.channel[1].CANCFG.cmode = 0; // Normal Mode for communicating
 	AVR32_CANIF.channel[1].CANCFG.sm    = 1; // Sampling Method
 	AVR32_CANIF.channel[1].CANCFG.sjw   = 1; // Sync Jump width
 	AVR32_CANIF.channel[1].CANCFG.prs   = 1; // Propagation segment
@@ -110,7 +123,7 @@ void can_init(void) {
 	AVR32_GPIO.port[2].pmr0 = 1<<22; AVR32_GPIO.port[2].pmr1 =     0; AVR32_GPIO.port[2].pmr2 = 0; // Port C Pin 22 CANTX MODE B
 	// CANIF channel 0
 	AVR32_CANIF.channel[0].CANCFG.ovrm  = 1; // Overrun Mode
-	AVR32_CANIF.channel[0].CANCFG.cmode = 2; // Loopback Mode
+	AVR32_CANIF.channel[0].CANCFG.cmode = 0; // Normal Mode for communicating
 	AVR32_CANIF.channel[0].CANCFG.sm    = 1; // Sampling Method
 	AVR32_CANIF.channel[0].CANCFG.sjw   = 1; // Sync Jump width
 	AVR32_CANIF.channel[0].CANCFG.prs   = 1; // Propagation segment
@@ -134,7 +147,7 @@ void can_receive_ch0(void) {
 	}
 }
 
-void can_send_ch0(int led, char data[]) {
+void can_send_ch0(char data[64]) {
 	/*
 	 *  Communication with TI LMM:
 	 *		Frame initialisation bit
@@ -162,7 +175,8 @@ void can_send_ch0(int led, char data[]) {
 	if (AVR32_CANIF.channel[0].CANSR.ces == 1) { // check if channel is enabled
 		// For debugging: a unused GPIO PIN can be set to 1 when this case is true
 		while(AVR32_CANIF.channel[0].CANSR.ts == 1){ // while sending
-			// TODO: Add code to transmit message
+			for (int msgL = 0; msgL >= sizeof(data); msgL++) { // 
+			}
 			
 		}
 	}
@@ -198,23 +212,79 @@ void can_send_ch1(void) {
 	}
 }
 
-// DAC Schnittstelle initialisieren
+// DAC Schnittstelle initialisieren channel A
 void dac_init(void) {
+	/*
+	 * PIN PORT INTERFACE USAGE MODE
+	 */
+	
 	// GPIO Ports initialisieren
+	AVR32_GPIO.port[0].gper = 0<<20; 
+	AVR32_GPIO.port[0].pmr0 = 0; AVR32_GPIO.port[0].pmr1 = 1<<20; AVR32_GPIO.port[0].pmr2 = 0; // PORT A PIN 20 MODE C 
 	
 	// Interne Register initialisieren
+	AVR32_DACIFB0.CR.arae = 1;
+	AVR32_DACIFB0.CR.trae = 0;
+	AVR32_DACIFB0.CR.aoe = 1; // Enable channel A
+	AVR32_DACIFB0.CR.en = 1; // Enable DAC
 }
 
 // ADC Schnittstelle initialisieren
 void adc_init(void) {
 	// GPIO Ports initialisieren
+	AVR32_GPIO.port[0].gper = 0 << 8;
+	AVR32_GPIO.port[0].pmr0 = 0; AVR32_GPIO.port[0].pmr1 = 0; AVR32_GPIO.port[0].pmr2 = 0; // PORT A PIN 8 MODE A
 	
 	// Interne Register initialisieren
+	AVR32_ADCIFA.CR.tstart = 1;
+	AVR32_ADCIFA.CR.tstop = 1;
+	AVR32_ADCIFA.CR.soc0 = 1;
+	AVR32_ADCIFA.CFG.adcen = 0;
+	AVR32_ADCIFA.CFG.muxset = 0;
+	AVR32_ADCIFA.CFG.exref = 0;
+	AVR32_ADCIFA.CFG.shd = 1;
+	AVR32_ADCIFA.CFG.rs = 1;
+	AVR32_ADCIFA.CFG.frm = 0;
+	AVR32_ADCIFA.CFG.ssmq = 1;
+	AVR32_ADCIFA.CFG.sleep = 0;
+	AVR32_ADCIFA.CFG.adcen = 1;
+}
+
+void check_for_ucd_command(void) {
+	ucd_message_receive = can_receive_ch1();
+	// 1 = Einstellung
+	// 2 = Strom messen
+	switch (decodeUCDMessage(ucd_message_receive)) {
+		case 1: decodeUCDSettings(ucd_message_receive);
+		case 2: getCurrent(ucd_message_receive)
+	}
+}
+
+int decodeUCDMessage(char data[64]) {
+	int mode = 0;
+	
+	//TODO: ADD CODE TO DECODE MESSAGE
+	
+	return mode;
+}
+
+int decodeUCDSettings(char data[64]) {
+	int mode = 0;
+	
+	//TODO: ADD CODE TO DECODE MESSAGE
+	
+	return mode;
+}
+
+float getCurrent(char data[64]) {
+	float current = 0.00;
+	
+	// TODO: ADC messung durchführen, ergebnis in eine variable speichern und wieder an das UCD senden
 }
 
 int main(void) {
     init();
     while (1) {
+		check_for_ucd_command();
     }
 }
-
